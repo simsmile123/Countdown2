482aa900f36567edca4a10b4b0cda14d5774c0c7
Simrith Ranjan <simrith.ranjan@gmail.com>
2024-04-15 15:54:30 -0400

    third i think this has everything but the buttons for green/red

--- old-trivia-app/src/App.jsx
+++ old-trivia-app/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import TriviaApp from "./components/TriviaApp";
 import './App.css'
 
 function App() {
@@ -8,10 +7,7 @@ function App() {
 
   return (
     <>
-      <TriviaApp>
-        
-      </TriviaApp>
-
+    <TriviaApp />
     </>
   )
 }
